14bf5479b6670ad872efc5fab45d18b347f36528
Hélio Carlitos António <heliocarlitosantonio@gmail.com>
2025-12-04 19:51:32 +0200

    ...

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true
+  // Não precisa de appDir
+}
 
-export default nextConfig;
+export default nextConfig
