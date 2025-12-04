0d798fe01ba2877cee39860a220ce66bba5d85ac
Hélio Carlitos António <heliocarlitosantonio@gmail.com>
2025-12-05 01:01:04 +0200

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Herosection } from "@/components/herosection/herosection"
+import { Whychoose } from "@/components/whychoose/whychoose"
 
 export default function Home() {
   return (
     <main>
       <Herosection />
+      <Whychoose />
     </main>
   )
 }
---
+++ src/components/whychoose/whychoose.tsx
@@ -0,0 +1,42 @@
+import "./whychoose.css"
+export function Whychoose() {
+  return (
+    <>
+      <section className="whychoose">
+        <div className="container">
+          <div className="intro">
+            <p className="title">Porque Escolher EcoNest</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <p className="c-text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo nam recusandae optio est illum vero et
+            repudiandae laborum nulla repellendus.
+          </p>
+        </div>
+
+        <div className="container">
+          <div className="container-child">
+            <div className="box">
+              <p className="number">500+</p>
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+            <div className="box">
+              <p className="number">15K+</p>
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+          </div>
+          <div className="container-child">
+            <div className="box">
+              <p className="number">85%</p>
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+            <div className="box">
+              <p className="number">900+</p>
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
